8bdb975d12371e8f155656c03ed2d46cf6ecfb57
Matheus Victor <matheusvictor.salles@gmail.com>
2022-07-13 22:50:38 -0300

    WIP: Refatora método recursivo

--- src/cArvoreQuad.cpp
+++ src/cArvoreQuad.cpp
@@ -8,16 +8,8 @@ cArvoreQuad::cArvoreQuad() {
     // this->raiz = construirArvorePorRecursao();
 }
 
-cArvoreQuad::cArvoreQuad(int largura, int altura) {
-    
-    cPonto pontoBase = cPonto(0, 0);
-    cQuadrante quadrante = cQuadrante();
-
-    quadrante.getPtoOposto();
-    this->raiz->setQuadrante(quadrante);
-    this->raiz = new cNo();
-    // this->raiz = construirArvorePorRecursao();
-    
+cArvoreQuad::cArvoreQuad(int largura, int altura, cPonto pBase, cPonto pOposto) {
+    this->raiz = construirArvorePorRecursao(largura, altura, pBase, pOposto);
 }
 
 void cArvoreQuad::setRaiz(cNo* n) {
@@ -28,11 +20,32 @@ cNo* cArvoreQuad::getRaiz() {
     return this->raiz;
 }
 
-cNo* cArvoreQuad::construirArvorePorRecursao(int largura, int altura) {
+cNo* cArvoreQuad::construirArvorePorRecursao(int largura, int altura, cPonto pBase, cPonto pOposto) {
     
+    // cPonto     pontoBase = p;
+    cQuadrante quadrante = cQuadrante(pBase, pOposto, 0);
+
     if (largura == 1 && altura == 1) return nullptr; // chegou ao pixel, não precisa mais continuar a divisão
 
-    cNo *novoNo = new cNo();
-    if (novoNo == nullptr) return novoNo; // alocação mau-sucedida
+    cNo *novoNo = new cNo(largura, altura, pBase);
+    if (novoNo == nullptr) return novoNo; // alocação de novoNo mau-sucedida
+
+    // int p0 = p.getX();
+
+    cPonto pBFSuperiorEsquerdo   = cPonto(pBase.getX(), pBase.getY());
+    cPonto pBFSuperiorDireito    = cPonto();
+
+    cPonto pBFInferiorDireito    = cPonto();
+    cPonto pBFInferiorEsquerdo   = cPonto(quadrante.getPtoBase().getX(), (quadrante.getPtoOposto().getY() / 2) + 1);
+
+    // pBase.setXY(l, a);
+
+    // novoNo->setFilhoSuperiorEsquerdo(construirArvorePorRecursao(l, 0, pBFSuperiorEsquerdo, pBFSuperiorEsquerdo));
+    // novoNo->setFilhoSuperiorDireito(construirArvorePorRecursao(l, a));
+
+    // novoNo->setFilhoInferiorDireito(construirArvorePorRecursao(l, a));
+    // novoNo->setFilhoInferiorEsquerdo(construirArvorePorRecursao(l, a));
+
+    return novoNo;
 
 }
--- src/cArvoreQuad.h
+++ src/cArvoreQuad.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cNo.h"
+#include "cPonto.h"
 #include "cQuadrante.h"
 
 class cArvoreQuad { 	
@@ -8,12 +9,12 @@ class cArvoreQuad {
 private:
 	cNo *raiz;	// representa a imagem
 
-	cNo* construirArvorePorRecursao(int largura, int altura);
+	cNo* construirArvorePorRecursao(int largura, int altura, cPonto pBase, cPonto pOposto);
 			
 public:
 
 	cArvoreQuad();
-	cArvoreQuad(int altura, int largura);
+	cArvoreQuad(int altura, int largura, cPonto p, cPonto pOposto);
 	cArvoreQuad(unsigned char* imagem);
 	
 	~cArvoreQuad();
