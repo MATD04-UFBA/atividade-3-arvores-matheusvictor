8f06a191f86e41e7e14832225a63af5da829bb29
Matheus Victor <matheusvictor.salles@gmail.com>
2022-07-14 23:16:30 -0300

    Remove comentários

--- src/cArvoreQuad.cpp
+++ src/cArvoreQuad.cpp
@@ -10,7 +10,6 @@
 
 cArvoreQuad::cArvoreQuad() {
     this->raiz = nullptr;
-    // this->raiz = construirArvorePorRecursao();
 }
 
 cArvoreQuad::cArvoreQuad(int largura, int altura, cPonto pBase) {
@@ -31,23 +30,13 @@ cNo* cArvoreQuad::construirArvorePorRecursao(int _largura, int _altura, cPonto p
     int altura  = _altura;
 
     cPonto pOposto = cPonto(
-        (pBase.getX() + largura), // verificar se deve ser -1
-        (pBase.getY() + altura) // verificar se deve ser -1
+        (pBase.getX() + largura),
+        (pBase.getY() + altura)
     );
         
     cQuadrante quadrante = cQuadrante(pBase, pOposto, 0);
-    //quadrante->setNivel(nivel);
-    
-    std::cout << "========================================================" << std::endl;
-    std::cout << "NIVEL " << nivel << std::endl; //getNivel
-    std::cout << "CRIADO O Q" << numeroQuadrante << std::endl;
-    std::cout << "========================================================" << std::endl;
-
-    std::cout << "pBase quadrante = (" << quadrante.getPtoBase().getX() << ", " << quadrante.getPtoBase().getY() << ")" << std::endl;
-    std::cout << "pOposto quadrante = (" << quadrante.getPtoOposto().getX() << ", " << quadrante.getPtoOposto().getY() << ")" << std::endl;
-
-
     cNo *novoNo = new cNo(largura, altura, pBase);
+
     if (novoNo == nullptr) return novoNo; // alocação de novoNo mau-sucedida
 
     if (quadrante.getPtoBase().getX() == quadrante.getPtoOposto().getX() &&
@@ -73,18 +62,11 @@ cNo* cArvoreQuad::construirArvorePorRecursao(int _largura, int _altura, cPonto p
                                             ceil( (quadrante.getPtoBase().getX() + largura) / 2) + 1, 
                                             ceil( (quadrante.getPtoBase().getY() + altura) / 2) + 1
                                         );
-    
-    //std::cout << "pBase quadrante = (" << quadrante.getPtoBase().getX() << "), (" << quadrante.getPtoBase().getY() << ")" << std::endl;
-    //std::cout << "pOposto quadrante = (" << quadrante.getPtoOposto().getX() << "), (" << quadrante.getPtoOposto().getY() << ")" << std::endl;
-
-    // possivelmente aqui quebra a recursão
 
     novoNo->setFilhoSuperiorEsquerdo( construirArvorePorRecursao(floor(_largura / 2), _altura/2, q1, nivel + 1, 1) );
     novoNo->setFilhoSuperiorDireito( construirArvorePorRecursao((_largura / 2), (_altura/2), q2, nivel + 1, 2) );
     novoNo->setFilhoInferiorDireito( construirArvorePorRecursao((_largura / 2), (_altura/2), q3, nivel + 1, 3) );
     novoNo->setFilhoInferiorEsquerdo( construirArvorePorRecursao((_largura / 2), (_altura/2), q4, nivel + 1, 4) );
-
-    // pBase.setXY(largura, altura);
 
     return novoNo;
 }
